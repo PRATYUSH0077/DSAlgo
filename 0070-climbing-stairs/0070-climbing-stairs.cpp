7b224d1c87ea673db20c77d3ba7c92f96eeddc60
Pratyush Sinha <98221457+PRATYUSH0077@users.noreply.github.com>
2025-01-03 02:11:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (21.2%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -9,7 +9,17 @@ public:
         return dp[n]=pickone+picktwo;
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return fn(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return fn(n,dp);
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            int pickone=0,picktwo=0;
+            pickone=dp[i-1];
+            if(i>1)
+                picktwo=dp[i-2];
+            dp[i]=pickone+picktwo;
+        }
+        return dp[n];
     }
 };
