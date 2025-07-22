c2d2f6fb856c9b94537cb50de4d50b0143f83397
Pratyush Sinha <98221457+PRATYUSH0077@users.noreply.github.com>
2025-07-22 21:40:38 +0530

    Time: 8 ms (44.91%), Space: 13.3 MB (34.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -4,7 +4,6 @@ public:
         if(i==n && j==m){
             return 0;
         }else if(i==n && j<m){
-
             return m-j;
         }else if(j==m && i<n){
             return n-i;
@@ -25,9 +24,35 @@ public:
     }
     int minDistance(string s, string t) {
         int n=s.size(),m=t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        int ans= lcs(0,0,n,m,s,t,dp);
-        return ans;
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
+        // int ans= lcs(0,0,n,m,s,t,dp);
+        // return ans;
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int j=0;j<=m;j++){
+            dp[n][j]=m-j;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][m]=n-i;
+        }
+        dp[n][m]=0;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans=INT_MAX;
+                if(s[i]==t[j]){
+                    ans=dp[i+1][j+1];
+                }else{
+                    ans=min(ans,1+dp[i+1][j]);
+                    ans=min(ans,1+dp[i+1][j+1]);
+                    ans=min(ans,1+dp[i][j+1]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
+
+
         return 0;
     }
 };
