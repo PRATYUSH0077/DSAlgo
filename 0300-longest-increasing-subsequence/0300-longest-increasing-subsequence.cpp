b0974c8e4504ad15c9942509036ecb5c3b2c5f46
Pratyush Sinha <98221457+PRATYUSH0077@users.noreply.github.com>
2025-07-26 00:33:52 +0530

    Time: 513 ms (25.59%), Space: 296.3 MB (31.04%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -8,7 +8,7 @@ public:
         if(dp[i][j+1]!=-1){
             return dp[i][j+1];
         }
-        int LIS=0,include=0,not_include=0;
+        int include=0,not_include=0;
         if(j==-1 || nums[i]>nums[j]){
             include=1+rec(i+1,i,n,nums,dp);
             
@@ -18,7 +18,24 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int len=nums.size();
-        vector<vector<int>> dp(len,vector<int>(len+1,-1));
-        return rec(0,-1,len,nums,dp);
+        // vector<vector<int>> dp(len,vector<int>(len+1,-1));
+        // return rec(0,-1,len,nums,dp);
+
+
+        // bcz rec(i,j)-> mapped to dp[i][j+1] for -1 case shift
+        vector<vector<int>> dp(len+1,vector<int>(len+1,0));
+        for(int i=len-1;i>=0;i--){
+            for(int j=-1;j<len;j++){
+                int include=0,not_include=0;
+                if(j==-1 || nums[i]>nums[j]){
+                    include=1+dp[i+1][i+1];
+                }
+                // if(j>-1)
+                    not_include=dp[i+1][j+1];
+                dp[i][j+1]=max(include,not_include);
+            }
+        }
+        return dp[0][0];
+
     }
 };
