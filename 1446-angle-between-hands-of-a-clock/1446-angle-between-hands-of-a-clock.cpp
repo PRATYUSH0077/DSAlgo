9489e33bf4850e094264ac415aa5b6125b00e443
Pratyush Sinha <98221457+PRATYUSH0077@users.noreply.github.com>
2025-09-29 22:45:17 +0530

    Time: 0 ms (100%), Space: 8.2 MB (9.85%) - LeetHub

--- 1446-angle-between-hands-of-a-clock/1446-angle-between-hands-of-a-clock.cpp
+++ 1446-angle-between-hands-of-a-clock/1446-angle-between-hands-of-a-clock.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     double angleClock(int h, int m) {
-        double angle_m=m/60.0*360;
+        double angle_m=(m/60.0)*360;
         double angle_h=h*30+(( m/60.0)*30);
         double res=abs(angle_m-angle_h);
 
